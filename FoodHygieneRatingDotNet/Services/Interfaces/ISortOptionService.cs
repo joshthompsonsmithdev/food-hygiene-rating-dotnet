f82f3ee5298d58d84df8d0a3e94a404df504df77
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:47:22 +0100

    #10 - Implemented Sort Option API wrapper.

---
+++ FoodHygieneRatingDotNet/Services/Interfaces/ISortOptionService.cs
@@ -0,0 +1,15 @@
+﻿using FoodHygieneRatingDotNet.Entities;
+using FoodHygieneRatingDotNet.Http;
+using System.Threading.Tasks;
+
+namespace FoodHygieneRatingDotNet.Services.Interfaces
+{
+    public interface ISortOptionService
+    {
+        /// <summary>
+        /// Retrieves details of all SortOptions.
+        /// </summary>
+        /// <returns>Returns details of all SortOptions.</returns>
+        Task<FhrResponse<SortOptions>> GetAsync();
+    }
+}

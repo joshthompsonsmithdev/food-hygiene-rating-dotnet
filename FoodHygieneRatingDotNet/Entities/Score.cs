7d3035c04227194af132d6bdceaf458099309411
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:52:39 +0100

    #13 - Implemented Score Descriptor API wrapper.

---
+++ FoodHygieneRatingDotNet/Entities/Score.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodHygieneRatingDotNet.Entities
+{
+    public class Score
+    {
+        public int Hygiene { get; set; }
+
+        public int Structural { get; set; }
+
+        public int ConfidenceInManagement { get; set; }
+    }
+}
---
+++ FoodHygieneRatingDotNet/Entities/ScoreDescriptor.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodHygieneRatingDotNet.Entities
+{
+    public class ScoreDescriptor : EntityBase
+    {
+        public int Id { get; set; }
+
+        public string ScoreCategory { get; set; }
+
+        public int Score { get; set; }
+
+        public string Description { get; set; }
+    }
+}

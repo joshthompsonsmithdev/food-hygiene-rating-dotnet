3733585c6fabbfcee4ecb5529840f218d5780e6c
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:23:24 +0100

    #3 - Implemented SchemeType API wrapper.

---
+++ FoodHygieneRatingDotNet/Services/SchemeTypeService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using FoodHygieneRatingDotNet.Entities;
+using FoodHygieneRatingDotNet.Http;
+using FoodHygieneRatingDotNet.Services.Interfaces;
+
+namespace FoodHygieneRatingDotNet.Services
+{
+    /// <summary>
+    /// Retrieves various data that's specific to scheme types.
+    /// </summary>
+    public class SchemeTypeService : ISchemeTypeService
+    {
+        private readonly IHttpService _httpService;
+
+        public SchemeTypeService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        /// <inheritdoc />
+        public async Task<FhrResponse<SchemeTypes>> GetAsync()
+        {
+            var url = _httpService.BuildApiUrl(HttpConstants.Endpoints.SchemeTypes);
+
+            var response = await _httpService.GetAsync<SchemeTypes>(url);
+
+            return response;
+        }
+    }
+}

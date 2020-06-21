9807d3ed2ffe0f2ad00d009a4493ea35018284df
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:48:41 +0100

    #11 - Implemented Rating Operator API wrapper.

---
+++ FoodHygieneRatingDotNet/Entities/RatingOperators.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FoodHygieneRatingDotNet.Entities
+{
+    public class RatingOperators : EntityBase
+    {
+        [JsonProperty("ratingOperator")]
+        public List<RatingOperator> Data { get; set; }
+    }
+}

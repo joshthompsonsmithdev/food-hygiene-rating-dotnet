198e73b3dc3ead2b11e22707f8fc98100580de0d
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:49:33 +0100

    #12 - Implemented Rating API wrapper.

---
+++ FoodHygieneRatingDotNet/Services/IRatingService.cs
@@ -0,0 +1,15 @@
+﻿using FoodHygieneRatingDotNet.Entities;
+using FoodHygieneRatingDotNet.Http;
+using System.Threading.Tasks;
+
+namespace FoodHygieneRatingDotNet.Services.Interfaces
+{
+    public interface IRatingService
+    {
+        /// <summary>
+        /// Retrieves details of all Ratings.
+        /// </summary>
+        /// <returns>Returns details of all Ratings.</returns>
+        Task<FhrResponse<Ratings>> GetAsync();
+    }
+}

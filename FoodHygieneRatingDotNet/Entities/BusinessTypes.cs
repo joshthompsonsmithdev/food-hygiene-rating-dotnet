cb33c3338f54019fec4044258a551bfa253bb3e5
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-21 20:34:57 +0100

    #5 - Implemented Business Type API wrapper.

---
+++ FoodHygieneRatingDotNet/Entities/BusinessTypes.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FoodHygieneRatingDotNet.Entities
+{
+    public class BusinessTypes : EntityBase
+    {
+        [JsonProperty("businesstypes")]
+        public List<BusinessType> Data { get; set; }
+    }
+}

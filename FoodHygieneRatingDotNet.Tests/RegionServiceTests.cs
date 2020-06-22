aaf28324681f6dc1273d7450cf47425aae20d9c7
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-22 21:45:18 +0100

    #15 - Implemented Region API tests.

---
+++ FoodHygieneRatingDotNet.Tests/RegionServiceTests.cs
@@ -0,0 +1,394 @@
+﻿using Moq;
+using NUnit.Framework;
+using FoodHygieneRatingDotNet.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoodHygieneRatingDotNet.Tests
+{
+    public class RegionServiceTests
+    {
+        public class GetAsyncMethod : RegionServiceTestsBase
+        {
+            [Test]
+            public async Task ProvidesCorrectApiUrl()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync();
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/Regions";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync();
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ regions: [{ name: \"Foo\" }] }", "Foo")]
+            [TestCase("{ regions: [{ name: \"Bar\" }] }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync();
+
+                // Assert
+                Assert.AreEqual(response.Response.Data[0].Name, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync();
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+
+        public class GetAsyncIntMethod : RegionServiceTestsBase
+        {
+            [TestCase(1)]
+            [TestCase(5)]
+            public async Task ProvidesCorrectApiUrl(int id)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync(id);
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/Regions/{id}";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync(0);
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ name: \"Foo\" }", "Foo")]
+            [TestCase("{ name: \"Bar\" }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync(0);
+
+                // Assert
+                Assert.AreEqual(response.Response.Name, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync(0);
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+
+        public class GetBasicAsyncMethod : RegionServiceTestsBase
+        {
+            [Test]
+            public async Task ProvidesCorrectApiUrl()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetBasicAsync();
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/Regions/basic";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetBasicAsync();
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ regions: [{ name: \"Foo\" }] }", "Foo")]
+            [TestCase("{ regions: [{ name: \"Bar\" }] }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetBasicAsync();
+
+                // Assert
+                Assert.AreEqual(response.Response.Data[0].Name, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetBasicAsync();
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+
+        public class GetPagedAsyncMethod : RegionServiceTestsBase
+        {
+            [TestCase(1, 20)]
+            [TestCase(6, 90)]
+            public async Task ProvidesCorrectApiUrl(int page, int size)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetPagedAsync(page, size);
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/Regions/{page}/{size}";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetPagedAsync(0, 0);
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ regions: [{ name: \"Foo\" }] }", "Foo")]
+            [TestCase("{ regions: [{ name: \"Bar\" }] }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetPagedAsync(1, 1);
+
+                // Assert
+                Assert.AreEqual(response.Response.Data[0].Name, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetPagedAsync(1, 1);
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+
+        public class GetPagedBasicAsyncMethod : RegionServiceTestsBase
+        {
+            [TestCase(1, 20)]
+            [TestCase(6, 90)]
+            public async Task ProvidesCorrectApiUrl(int page, int size)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetPagedBasicAsync(page, size);
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/Regions/basic/{page}/{size}";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetPagedBasicAsync(0, 0);
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ regions: [{ name: \"Foo\" }] }", "Foo")]
+            [TestCase("{ regions: [{ name: \"Bar\" }] }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetPagedBasicAsync(1, 1);
+
+                // Assert
+                Assert.AreEqual(response.Response.Data[0].Name, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetPagedBasicAsync(1, 1);
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+    }
+}

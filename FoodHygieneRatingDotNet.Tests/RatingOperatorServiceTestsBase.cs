03cf706daffac2587af2c22e3e1bccda9d46f29c
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-22 21:47:17 +0100

    #19 - Implemented Rating Operator API tests.

---
+++ FoodHygieneRatingDotNet.Tests/RatingOperatorServiceTestsBase.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using FoodHygieneRatingDotNet.Http;
+using FoodHygieneRatingDotNet.Services;
+
+namespace FoodHygieneRatingDotNet.Tests
+{
+    public abstract class RatingOperatorServiceTestsBase : ServiceTestsBase
+    {
+        protected RatingOperatorService Service { get; }
+
+        protected IHttpService HttpService { get; }
+
+        protected Mock<IHttpProvider> MockHttpProvider { get; }
+
+        protected RatingOperatorServiceTestsBase()
+        {
+            MockHttpProvider = new Mock<IHttpProvider>();
+            HttpService = new HttpService(MockHttpProvider.Object);
+            Service = new RatingOperatorService(HttpService);
+        }
+    }
+}

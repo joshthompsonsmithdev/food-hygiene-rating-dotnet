2fe7859d00ae645a6cad4d649b1ce25592a33730
Josh Thompson-Smith <joshthompsonsmith@itg.co.uk>
2020-06-22 21:48:33 +0100

    #21 - Implemented Sort Option API tests.

---
+++ FoodHygieneRatingDotNet.Tests/SortOptionServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Moq;
+using NUnit.Framework;
+using FoodHygieneRatingDotNet.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoodHygieneRatingDotNet.Tests
+{
+    public class SortOptionServiceTests
+    {
+        public class GetAsyncMethod : SortOptionServiceTestsBase
+        {
+            [Test]
+            public async Task ProvidesCorrectApiUrl()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync();
+
+                // Assert
+                var expected = $"{HttpConstants.FhrHttpProtocol}://{HttpConstants.FhrBaseApiUrl}/SortOptions";
+                MockHttpProvider.Verify(http => http.GetAsync(It.Is<string>(response => response == expected), It.IsAny<IDictionary<string, string>>()));
+            }
+
+            [Test]
+            public async Task ProvidesCorrectApiHeaders()
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(string.Empty)));
+
+                // Act
+                await Service.GetAsync();
+
+                // Assert
+                const string expectedKey = "x-api-version";
+                const string expectedValue = "2";
+                
+                MockHttpProvider
+                    .Verify(http => http.GetAsync(It.IsAny<string>(), It.Is<IDictionary<string, string>>
+                    (
+                        response =>
+                        response[expectedKey] != null &&
+                        response[expectedKey] == expectedValue
+                    )));
+            }
+
+            [TestCase("{ sortOptions: [{ sortOptionName: \"Foo\" }] }", "Foo")]
+            [TestCase("{ sortOptions: [{ sortOptionName: \"Bar\" }] }", "Bar")]
+            public async Task SerialisesContentStringToCorrectObject(string content, string expectedName)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync();
+
+                // Assert
+                Assert.AreEqual(response.Response.Data[0].SortOptionName, expectedName);
+            }
+
+            [TestCase("some failing string 1")]
+            [TestCase("some failing string 2")]
+
+            public async Task SerialisesContentStringToNullWhenInvalid(string content)
+            {
+                // Arrange
+                MockHttpProvider
+                    .Setup(http => http.GetAsync(It.IsAny<string>(), It.IsAny<IDictionary<string, string>>()))
+                    .Returns(Task.FromResult(GenerateHttpResponse(content)));
+
+                // Act
+                var response = await Service.GetAsync();
+
+                // Assert
+                Assert.IsNull(response.Response);
+            }
+        }
+    }
+}
---
+++ FoodHygieneRatingDotNet.Tests/SortOptionServiceTestsBase.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using FoodHygieneRatingDotNet.Http;
+using FoodHygieneRatingDotNet.Services;
+
+namespace FoodHygieneRatingDotNet.Tests
+{
+    public abstract class SortOptionServiceTestsBase : ServiceTestsBase
+    {
+        protected SortOptionService Service { get; }
+
+        protected IHttpService HttpService { get; }
+
+        protected Mock<IHttpProvider> MockHttpProvider { get; }
+
+        protected SortOptionServiceTestsBase()
+        {
+            MockHttpProvider = new Mock<IHttpProvider>();
+            HttpService = new HttpService(MockHttpProvider.Object);
+            Service = new SortOptionService(HttpService);
+        }
+    }
+}
